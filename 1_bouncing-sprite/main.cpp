3003d13c5c1b7396246680d0d6e3e6d6cbea1b82
Endiliey <endiliey@gmail.com>
2017-02-15 23:59:02 +0800

    add bouncing sprite game

---
+++ 1_bouncing-sprite/main.cpp
@@ -0,0 +1,46 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(640, 480), "First SFML!");
+
+	// create a texture and sprite
+	sf::Texture endiTexture;
+	endiTexture.loadFromFile("endi.png");
+	
+	sf::Sprite endi(endiTexture);
+	sf::Vector2u size = endiTexture.getSize();
+	endi.setOrigin(size.x / 2, size.y / 2);
+	sf::Vector2f increment(0.1f, 0.1f);
+		
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+		
+		if((endi.getPosition().x + (size.x / 2) > window.getSize().x && increment.x > 0) || (endi.getPosition().x - (size.x / 2) < 0 && increment.x < 0))
+		{
+			// Reverse direction on X axis
+			increment.x = -increment.x;
+		}
+
+		if((endi.getPosition().y + (size.y / 2) > window.getSize().y && increment.y > 0) || (endi.getPosition().y - (size.y / 2) < 0 && increment.y < 0))
+		{
+			// Reverse direction on Y axis
+			increment.y = -increment.y;
+		}
+		
+		// move randomly with some increment	
+		endi.setPosition(endi.getPosition() + increment);
+
+		window.clear(sf::Color::White);
+		window.draw(endi);
+		window.display();
+	}
+}
---
+++ 1_bouncing-sprite/play.sh
@@ -0,0 +1,3 @@
+g++ -std=c++11 -c main.cpp
+g++ main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
+./sfml-app
